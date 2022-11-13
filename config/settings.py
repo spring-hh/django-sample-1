d829bc0e32d11f1872b427ae8232c6a5212a8c5f
spring-hh <spring.hh.shu@gmail.com>
2022-11-13 13:20:18 +0000

    topページの作成

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # top app
+    "top",
     # 非同期処理
     "asyncapp",
     "django_celery_results",
--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("asyncapp.urls")),
+    path("", include("top.urls")),
+    path("asyncapp/", include("asyncapp.urls")),
 ]
