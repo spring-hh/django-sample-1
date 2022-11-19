6163d244ee1a9922f79e792627d23896da9411b8
spring-hh <spring.hh.shu@gmail.com>
2022-11-19 14:56:33 +0000

    run black

--- dragdrop2/views.py
+++ dragdrop2/views.py
@@ -7,13 +7,13 @@ def drapdrop2(request):
         # get post body
         body = request.body
         # decode body json
-        body_unicode = body.decode('utf-8')
+        body_unicode = body.decode("utf-8")
         body_json = json.loads(body_unicode)
 
         for item in body_json:
-            print(item.get('id'))
-            print(item.get('name'))
-            print(item.get('target'))
-            print(item.get('editFlg'))
+            print(item.get("id"))
+            print(item.get("name"))
+            print(item.get("target"))
+            print(item.get("editFlg"))
 
     return render(request, "dragdrop2.html")
